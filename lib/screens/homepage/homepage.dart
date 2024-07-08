bbda567625d9b25241033277a1f4a1139882e08a
canruso <canruso@berkeley.edu>
2024-07-08 15:33:55 +0300

    location prompting beta code

--- lib/screens/homepage/homepage.dart
+++ lib/screens/homepage/homepage.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:my_flutter_app/screens/user_profile/user_profile.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -11,8 +13,27 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   late GoogleMapController mapController;
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+  String? _profileImageUrl;
 
-  final LatLng _center = const LatLng(45.521563, -122.677433);
+  final LatLng _center = const LatLng(37.8715, -122.2730); // our campus :)
+
+  @override
+  void initState() {
+    super.initState();
+    _loadUserProfile();
+  }
+
+  void _loadUserProfile() async {
+    User? user = _auth.currentUser;
+    if (user != null) {
+      DocumentSnapshot userProfile = await _firestore.collection('users').doc(user.uid).get();
+      setState(() {
+        _profileImageUrl = userProfile['imageUrl'];
+      });
+    }
+  }
 
   void _onMapCreated(GoogleMapController controller) {
     mapController = controller;
@@ -25,16 +46,21 @@ class _HomePageState extends State<HomePage> {
         title: const Text('Shuffl'),
         actions: [
           IconButton(
-            icon: const Icon(Icons.account_circle), // PULL USER PROFILE PP HERE 
+            icon: CircleAvatar(
+              backgroundImage: _profileImageUrl != null && _profileImageUrl!.isNotEmpty
+                  ? NetworkImage(_profileImageUrl!)
+                  : const AssetImage('assets/icons/ShuffleLogo.jpeg') as ImageProvider,
+            ),
             onPressed: () {
-              Navigator.push(context,
-              MaterialPageRoute(builder: (context) => const UserProfile()));
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => const UserProfile()),
+              );
             },
           )
         ],
       ),
-      drawer: const Drawer(
-      ),
+      drawer: const Drawer(),
       body: Column(
         children: [
           Padding(
@@ -66,7 +92,7 @@ class _HomePageState extends State<HomePage> {
               onMapCreated: _onMapCreated,
               initialCameraPosition: CameraPosition(
                 target: _center,
-                zoom: 11.0,
+                zoom: 15.0,
               ),
             ),
           ),
@@ -75,3 +101,132 @@ class _HomePageState extends State<HomePage> {
     );
   }
 }
+
+
+/*
+import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:my_flutter_app/screens/user_profile/user_profile.dart';
+import 'package:location/location.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  late GoogleMapController mapController;
+  final Location location = Location();
+  bool _serviceEnabled = false;
+  PermissionStatus _permissionGranted = PermissionStatus.denied;
+  LocationData? _locationData;
+  final LatLng _initialPosition = const LatLng(37.8715, -122.2730); // our campus :) 
+
+  @override
+  void initState() {
+    super.initState();
+    _getLocationPermission();
+  }
+
+  void _onMapCreated(GoogleMapController controller) {
+    mapController = controller;
+    if (_locationData != null) {
+      mapController.animateCamera(CameraUpdate.newLatLng(
+        LatLng(_locationData!.latitude!, _locationData!.longitude!),
+      ));
+    }
+  }
+
+  Future<void> _getLocationPermission() async {
+    print('Requesting location service...');
+    _serviceEnabled = await location.serviceEnabled();
+    if (!_serviceEnabled) {
+      _serviceEnabled = await location.requestService();
+      if (!_serviceEnabled) {
+        print('Location service not enabled');
+        return;
+      }
+    }
+
+    print('Requesting location permission...');
+    _permissionGranted = await location.hasPermission();
+    if (_permissionGranted == PermissionStatus.denied) {
+      _permissionGranted = await location.requestPermission();
+      if (_permissionGranted != PermissionStatus.granted) {
+        print('Location permission not granted');
+        return;
+      }
+    }
+
+    _locationData = await location.getLocation();
+    if (_locationData != null) {
+      mapController.animateCamera(CameraUpdate.newLatLng(
+        LatLng(_locationData!.latitude!, _locationData!.longitude!),
+      ));
+      print('Location obtained: ${_locationData!.latitude}, ${_locationData!.longitude}');
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Shuffl'),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.account_circle),
+            onPressed: () {
+              Navigator.push(context,
+              MaterialPageRoute(builder: (context) => const UserProfile()));
+            },
+          )
+        ],
+      ),
+      drawer: const Drawer(),
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: TextField(
+              decoration: InputDecoration(
+                hintText: 'Enter pick-up location',
+                prefixIcon: const Icon(Icons.location_on),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(10),
+                ),
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: TextField(
+              decoration: InputDecoration(
+                hintText: 'Enter destination',
+                prefixIcon: const Icon(Icons.location_on),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(10),
+                ),
+              ),
+            ),
+          ),
+          Expanded(
+            child: GoogleMap(
+              onMapCreated: _onMapCreated,
+              initialCameraPosition: CameraPosition(
+                target: _initialPosition,
+                zoom: 15.0,
+              ),
+              myLocationEnabled: true,
+              myLocationButtonEnabled: true,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+*/
+
+
