3004d7f5e12abb8681b0dc3cb3560f9648be5d71
Evan Davis <antipk93@berkeley.edu>
2024-08-03 16:30:58 -0700

    will want a user struct/class for algs

---
+++ algorithms/user_struct.py
@@ -0,0 +1,8 @@
+#can switch languages if needed
+# https://docs.google.com/document/d/1pbHnPRzcoDpIrBWiLboWNT-GisBIx2kLrkx3rP0k9V4/edit
+# https://github.com/googlemaps
+class User:
+    x_coord = 0
+    y_coord = 0
+    x_dest = 0
+    y_dest = 0 #placeholder
