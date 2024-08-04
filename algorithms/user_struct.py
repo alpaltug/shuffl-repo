852fb15fa1264df68d293931f684053592008f0b
Evan Davis <antipk93@berkeley.edu>
2024-08-03 19:58:23 -0700

    extra link

--- algorithms/user_struct.py
+++ algorithms/user_struct.py
@@ -1,6 +1,7 @@
 #can switch languages if needed
 # https://docs.google.com/document/d/1pbHnPRzcoDpIrBWiLboWNT-GisBIx2kLrkx3rP0k9V4/edit
 # https://github.com/googlemaps
+# https://developers.google.com/maps/documentation/routes
 class User:
     x_coord = 0
     y_coord = 0
